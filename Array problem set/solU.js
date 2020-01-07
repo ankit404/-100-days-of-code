7ac6222a991e3fc16c4ca8da96b43b5b84bccb1d
Ankit Rai <38010918+ankit404@users.noreply.github.com>
2020-01-07 22:58:18 +0530

    Update solU.js
    just the write code

--- Array problem set/solU.js
+++ Array problem set/solU.js
@@ -1,4 +1,3 @@
-
 console.log("-----printing the reverse of array-----");
 function revArray(arr){
   for(var i=arr.length-1;i>=0;i--){
@@ -8,21 +7,43 @@ function revArray(arr){
 revArray([1,2,3,4]);
 
 console.log("--------return true, elements in array identical-----------");
-var k;
-function isUniform(rr){
-    var i,j;
-    i=0;
-    j=rr.length-1;
-    while(k<=rr.length){
-        if(rr[i]===rr[j]){
-            return true;
-        }else{
-            return false;
-        }
-    i++;
-    j--;
 
+function isUniform(r){
+var first=r[0];
+for(i=1;i<r.length;i++){
+    if(first!==r[i]){
+        return false;
     }
-
 }
-isUniform([1,1,1,1]);
+return true;
+}
+console.log(isUniform([1,2,1,1]));
+
+
+console.log("-------printing sum of an array-------");
+var sum=0;
+function sumArray(a){
+  for(i=0;i<a.length;i++){
+    sum=sum+a[i];
+  }
+  console.log("the sum of array is:"+" "+sum);
+}
+sumArray([1,2,3]);
+
+
+console.log("------------finding  max of the numbers-----------");
+var mx,j;
+function max(b){
+    j=b.length-1
+    mx=b[0];
+    while(j>=0){
+    for(var i=1;i<b.length;i++){
+        if(mx<b[i]){
+            mx=b[i];
+            }
+    }
+    j--;
+}
+console.log(mx +" "+"is the maximum number");
+}
+max([1,2,3,4,5]);
