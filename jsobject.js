0f6fb3b075f0cc0e55daf079cebcb2cf9350e59b
Ankit Rai <38010918+ankit404@users.noreply.github.com>
2020-01-10 23:38:29 +0530

    Add files via upload
    js exercise

---
+++ jsobject.js
@@ -0,0 +1,28 @@
+var movie = [{
+        title: "\"One punch man\"",
+        rating: "5 stars",
+        hasWatched: "seen"
+    },
+    {
+        title: "\"Naruto\"",
+        rating: "4.5 stars",
+        hasWatched: "not seen"
+    },
+    {
+        title: "\"Mob phyco 100\"",
+        rating: "5 stars",
+        hasWatched: "seen"
+    },
+    {
+        title: "\"Tokyo ghoul\"",
+        rating: "3.5 stars",
+        hasWatched: "not seen"
+    }
+];
+
+for (var i = 0; i < movie.length; i++) {
+
+    console.log("you have" + " " + movie[i].hasWatched + " " + movie[i].title + " - " + movie[i].rating);
+
+
+}
