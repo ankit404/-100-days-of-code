b51d53dd45e84697cc4a7abfd7decc01f74f66c0
Ankit Rai <38010918+ankit404@users.noreply.github.com>
2020-02-06 23:29:41 +0530

    Update index.js

--- Color game/index.js
+++ Color game/index.js
@@ -1,8 +1,27 @@
 var colors = [
-    "rgb()",
-    "rgb()",
-    "rgb()",
-    "rgb()",
-    "rgb()",
-    "rgb()",
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
 ]
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.querySelector("#colorDisplay");
+colorDisplay.textContent = pickedColor;
+for (var i = 0; i < squares.length; i++) {
+    // adding colors to sqauares
+    squares[i].style.backgroundColor = colors[i];
+    // add click listener to the squares
+    squares[i].addEventListener("click", function() {
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor) {
+            alert("correct!");
+        } else {
+            this.style.backgroundColor = "#909090";
+        }
+    });
+
+
+}
